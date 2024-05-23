09738b5e5a10d54178725bcdb531c3c72c3e746d
Felo esports <167418432+felloesports@users.noreply.github.com>
2024-05-23 14:47:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -104,23 +104,5 @@ window.onload = function() {
   // Call the fetchNews function when the page loads
   window.addEventListener('load', fetchNews);
 
-  let sections = document.querySelectorAll('section');
-  let navLinks = document.querySelectorAll('header nav a');
   
-  window.onscroll = () => {
-      sections.forEach(sec => {
-          let top = window.scrollY;
-          let offset = sec.offsetTop - 150;
-          let height = sec.offsetHeight;
-          let id = sec.getAttribute('id');
-  
-          if(top >= offset && top < offset + height) {
-              navLinks.forEach(links => {
-                  links.classList.remove('active');
-                  document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-              });
-          };
-      });
-  };
-
   
